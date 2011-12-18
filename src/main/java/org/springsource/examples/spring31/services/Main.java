6aa9197e7479a9f3ed43829bb81d482410abed2e
Josh Long <josh@joshlong.com>
2011-12-17 23:04:18 -0800

    initial commit

---
+++ src/main/java/org/springsource/examples/spring31/services/Main.java
@@ -0,0 +1,24 @@
+package org.springsource.examples.spring31.services;
+
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Date;
+
+public class Main {
+    static public void main(String[] args) throws Throwable {
+        AnnotationConfigApplicationContext annotationConfigApplicationContext =
+                new AnnotationConfigApplicationContext(ServicesConfiguration.class.getPackage().getName());
+        CustomerService customerService = annotationConfigApplicationContext.getBean(CustomerService.class);
+
+        Customer customer = customerService.createCustomer("Josh", "Long", new Date());
+        
+        customer  = customerService.getCustomerById( customer.getId()) ;
+        
+        customer = customerService.getCustomerById( customer.getId());
+
+
+
+    }
+}                           
+
